da07d41de4b4a65960b297869610e6bbb1071806
awooooool <45804108+awooooool@users.noreply.github.com>
2022-03-08 00:55:37 +0700

    fix(Reddit): subreddit browsing status

--- websites/R/Reddit/presence.ts
+++ websites/R/Reddit/presence.ts
@@ -142,7 +142,7 @@ presence.on("UpdateData", async () => {
 		const sub = document.querySelector("span._1GieMuLljOrqnVpRAwz7VP");
 		if (sub === null) {
 			const sub2 = document.querySelector(
-				"#SHORTCUT_FOCUSABLE_DIV > div:nth-child(4) > div > div > div > div._3ozFtOe6WpJEMUtxDOIvtU > div.q4a8asWOWdfdniAbgNhMh > div > div.QscnL9OySMkHhGudEvEya > div > div._3TG57N4WQtubLLo8SbAXVF > h2"
+				"#SHORTCUT_FOCUSABLE_DIV > div:nth-child(4) > div > div > div > div._3ozFtOe6WpJEMUtxDOIvtU > div.MSTY2ZpsdupobywLEfx9u > div._3JDs8KEQIXSMn1bTF2ZqJ_ > div.QscnL9OySMkHhGudEvEya > div._3I4Wpl_rl6oTm02aWPZayD > div._3TG57N4WQtubLLo8SbAXVF > h2"
 			);
 			presenceData.details = (await strings).browsing;
 			presenceData.state = !sub2 ? "Home" : sub2.textContent;
