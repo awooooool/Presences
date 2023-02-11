a6bbc946de4e6c31fc8693fe794088576117175f
awooooool <45804108+awooooool@users.noreply.github.com>
2023-02-11 23:05:09 +0700

    feat(Pi-hole): add presence for dashboard

--- websites/P/Pi-hole/presence.ts
+++ websites/P/Pi-hole/presence.ts
@@ -1,12 +1,21 @@
 const presence = new Presence({
-		clientId: "1073975411156979802",
-	});
+	clientId: "1073975411156979802",
+});
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		largeImageKey: "https://i.imgur.com/T0dkjxz.png",
-		state: `Hostname: ${document.querySelector("body > div.wrapper > header > nav > div > ul > li:nth-child(1) > p > code").textContent}`
+		largeImageKey: "https://i.imgur.com/T0dkjxz.png"
 	};
+
+	const path = document.location.pathname;
+	if (path === "/admin" || path.includes("/admin/index.php")) {
+		const stats = {
+			total: document.querySelector("#dns_queries_today").textContent,
+			blockedPercentage: document.querySelector("#percentage_blocked_today").textContent,
+		};
+		presenceData.details = "In dashboard";
+		presenceData.state = `Total queries: ${stats.total} | Blocked: ${stats.blockedPercentage}`;
+	}
 
 	presence.setActivity(presenceData);
 });
