049bb448bd1a0c45d9e7f9f46a5c69287ec4ed49
awooooool <45804108+awooooool@users.noreply.github.com>
2023-02-11 23:39:36 +0700

    feat(Pi-hole): add presence for query log

--- websites/P/Pi-hole/presence.ts
+++ websites/P/Pi-hole/presence.ts
@@ -4,17 +4,31 @@ const presence = new Presence({
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
-		largeImageKey: "https://i.imgur.com/T0dkjxz.png"
+		largeImageKey: "https://i.imgur.com/T0dkjxz.png",
 	};
 
 	const path = document.location.pathname;
 	if (path === "/admin" || path.includes("/admin/index.php")) {
 		const stats = {
 			total: document.querySelector("#dns_queries_today").textContent,
-			blockedPercentage: document.querySelector("#percentage_blocked_today").textContent,
+			blockedPercentage: document.querySelector("#percentage_blocked_today")
+				.textContent,
 		};
 		presenceData.details = "In dashboard";
 		presenceData.state = `Total queries: ${stats.total} | Blocked: ${stats.blockedPercentage}`;
+	} else if (path.includes("/admin/queries.php")) {
+		presenceData.details = "In query log";
+		const filter = document.querySelectorAll(
+			"tbody > [role='row']:nth-child(1) > [class='highlight']"
+		);
+		if (filter.length > 0) {
+			let smallText = "Filtering: ";
+			for (const [index, element] of filter.entries()) {
+				smallText = smallText + element.textContent;
+				if (index !== filter.length - 1) smallText = `${smallText} | `;
+			}
+			presenceData.state = smallText;
+		}
 	}
 
 	presence.setActivity(presenceData);
