0bc05fce19d83290effe7295eeaf7e1264539b59
awooooool <45804108+awooooool@users.noreply.github.com>
2023-02-11 23:42:42 +0700

    chore(Pi-hole): add comments

--- websites/P/Pi-hole/presence.ts
+++ websites/P/Pi-hole/presence.ts
@@ -4,10 +4,13 @@ const presence = new Presence({
 
 presence.on("UpdateData", async () => {
 	const presenceData: PresenceData = {
+			// Pi-hole logo
 			largeImageKey: "https://i.imgur.com/T0dkjxz.png",
 		},
 		path = document.location.pathname;
 	if (path === "/admin" || path.includes("/admin/index.php")) {
+		// In dashboard
+		// Query statistics
 		const stats = {
 			total: document.querySelector("#dns_queries_today").textContent,
 			blockedPercentage: document.querySelector("#percentage_blocked_today")
@@ -16,12 +19,16 @@ presence.on("UpdateData", async () => {
 		presenceData.details = "In dashboard";
 		presenceData.state = `Total queries: ${stats.total} | Blocked: ${stats.blockedPercentage}`;
 	} else if (path.includes("/admin/queries.php")) {
+		// In query log
 		presenceData.details = "In query log";
+		// For filtering function in query log
 		const filter = document.querySelectorAll(
 			"tbody > [role='row']:nth-child(1) > [class='highlight']"
 		);
+		// If there's active filtering in page
 		if (filter.length > 0) {
 			let smallText = "Filtering: ";
+			// Concat all active filter
 			for (const [index, element] of filter.entries()) {
 				smallText = smallText + element.textContent;
 				if (index !== filter.length - 1) smallText = `${smallText} | `;
